9dc151b6c04e6462795821b280a8355083006ea7
justidle2012 <1540168075@qq.com>
2019-11-11 10:59:59 +0800

    增加题号9201题解。

---
+++ Lv1/9201/9201.cpp
@@ -0,0 +1,11 @@
+//三角形面积
+//http://dev.xjoi.net/problem/9201
+#include <cstdio>
+#include <cmath>
+int main() {
+    int a,b;
+    scanf("%d%d", &a, &b);
+    printf("%d\n", (int)(0.5*a*b));
+
+    return 0;
+}
