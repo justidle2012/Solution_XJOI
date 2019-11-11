fb020c16a2abfd4a42f19003d5c9acdb8345f687
justidle2012 <1540168075@qq.com>
2019-11-11 21:05:09 +0800

    添加题号9213题解。

---
+++ Lv1/9213/9213.cpp
@@ -0,0 +1,14 @@
+//距离
+//http://dev.xjoi.net/problem/9213
+#include <cstdio>
+#include <cmath>
+
+//一道奥数题
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    printf("%d\n", a/(b+c)*d);
+
+    return 0;
+}
