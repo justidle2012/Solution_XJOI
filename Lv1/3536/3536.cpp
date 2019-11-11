471c024ff0aa570d534302e3ed57a8ba0c1f0abe
justidle2012 <1540168075@qq.com>
2019-11-11 21:43:04 +0800

    添加题号3536题解。

---
+++ Lv1/3536/3536.cpp
@@ -0,0 +1,18 @@
+//??
+//http://dev.xjoi.net/problem/8370
+#include <cstdio>
+#include <cmath>
+
+//??????????????
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (0==n%2) {
+        printf("even\n");
+    } else {
+        printf("odd\n");
+    }
+
+    return 0;
+}
