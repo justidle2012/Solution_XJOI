65186a50a71eca80004ee41aafb88ae2813e02b5
justidle2012 <1540168075@qq.com>
2019-11-11 15:32:25 +0800

    添加题号9299题解。

---
+++ Lv1/9299/9299.cpp
@@ -0,0 +1,13 @@
+//简单解方程
+//http://dev.xjoi.net/problem/9299
+#include <cstdio>
+
+int main() {
+    int r1, s;
+    scanf("%d %d", &r1, &s);
+
+    int r2 = 2*s-r1;
+    printf("%d\n", r2);
+
+    return 0;
+}
