894b630167b77397257006d01cfe55f013c8f516
justidle2012 <1540168075@qq.com>
2019-11-11 14:34:20 +0800

    添加题号1000题解。

---
+++ Lv1/1000/1000.cpp
@@ -0,0 +1,11 @@
+//萌新关爱之-A+B Problem
+//http://www.hzxjhs.com:83/problem/1000
+#include <cstdio>
+
+int main() {
+    long long a,b;
+    scanf("%lld %lld", &a, &b);
+    printf("%lld\n", a+b);
+
+    return 0;
+}
