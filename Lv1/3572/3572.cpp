e2c92e6553e47a5c2bfc427ddf251341b7bd78b1
justidle2012 <1540168075@qq.com>
2019-11-11 17:20:28 +0800

    添加题号3572题解。

---
+++ Lv1/3572/3572.cpp
@@ -0,0 +1,12 @@
+//浮点除法
+//http://dev.xjoi.net/problem/3572
+#include <cstdio>
+#include <cmath>
+
+int main() {
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+    printf("%.3lf\n", a/b);
+
+    return 0;
+}
