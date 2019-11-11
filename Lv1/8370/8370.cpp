55d2d64239f1d81349eeed4844f5665438688cc4
justidle2012 <1540168075@qq.com>
2019-11-11 21:26:58 +0800

    添加题号8370题解。

--- Lv1/8370/8370.cpp
+++ Lv1/8370/8370.cpp
@@ -1,26 +1,32 @@
-//食物
+//??
 //http://dev.xjoi.net/problem/8370
 #include <cstdio>
 #include <cmath>
 
-bool judge(int n) {
-    double x = sqrt(n);
-    if (floor(x)==x) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
+//??????????????
 int main() {
     int n;
     scanf("%d", &n);
 
-    int i;
     int ans = 0;
-    for (i=1; i<=n; i++) {
-        if (false == judge(i)) {
-            ans++;
+    int a,b,c,d,e,f,g,h;
+    for (a=0; a<=n; a+=2) {//????????
+        for (b=0; b<=1; b++) {//???0??1?
+            for (c=0; c<=2; c++) {//???0??1??2?
+                for (d=1; d<=n; d+=2) {//???????
+                    for (e=0; e<=n; e+=4) {//???4????
+                        for (f=0; f<=3; f++) {//???0??1??2??3?
+                            for (g=0; g<=1; g++) {//???????????
+                                //??
+                                h = n-a-b-c-d-e-f-g;
+                                if (h>=0 && h%3==0) {
+                                    ans++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
