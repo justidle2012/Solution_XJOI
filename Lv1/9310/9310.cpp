4da2fa981248c4fe29d0e4ffbf7edb26bc1aad3f
justidle2012 <1540168075@qq.com>
2019-11-11 21:39:07 +0800

    添加题号9310题解。

---
+++ Lv1/9310/9310.cpp
@@ -0,0 +1,18 @@
+//??
+//http://dev.xjoi.net/problem/8370
+#include <cstdio>
+#include <cmath>
+
+//??????????????
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    printf("This is an integer.");
+    if (0==n%2) {
+        printf(" It's even.");
+    }
+    printf("\n");
+
+    return 0;
+}
