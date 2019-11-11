dbf374eee058dcd59ac0b2f97ddf238021593e14
justidle2012 <1540168075@qq.com>
2019-11-11 21:55:41 +0800

    添加题号3539题解。

---
+++ Lv1/3539/3539.cpp
@@ -0,0 +1,17 @@
+//美丽数
+//http://dev.xjoi.net/problem/3539
+#include <cstdio>
+#include <cmath>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (0==n%2 && n>50) {
+        printf("yes\n");
+    } else {
+        printf("no\n");
+    }
+
+    return 0;
+}
