06aa6bf743f58e507a84bd3e1a5e7ec7068509e2
justidle2012 <1540168075@qq.com>
2019-11-11 11:01:04 +0800

    增加题号8105题解。

---
+++ Lv1/8105/8105.cpp
@@ -0,0 +1,33 @@
+//发汽水
+//http://dev.xjoi.net/problem/8105
+#include <cstdio>
+#include <algorithm>
+
+const int MAXN = 1e5;
+int arr[MAXN];
+
+bool mycmp(int x, int y) {
+    return x>y;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int i;
+    for (i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    std::sort(arr, arr+n, mycmp);
+
+    for (i=0; i<n; i++) {
+        if (arr[i]<i) {
+            printf("%d\n", i);
+            return 0;
+        }
+    }
+    printf("%d\n", n);
+
+    return 0;
+}
