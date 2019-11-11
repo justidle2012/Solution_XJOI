f10319d193194138060a32c5d7432b8946d682e4
justidle2012 <1540168075@qq.com>
2019-11-11 16:09:21 +0800

    添加题号3541题解。

---
+++ Lv1/3541/3541.cpp
@@ -0,0 +1,14 @@
+//开根号
+//http://dev.xjoi.net/problem/3541
+#include <cstdio>
+#include <cmath>
+
+int main() {
+    long long n;
+    scanf("%lld", &n);
+
+    long long c = sqrt(n);
+    printf("%lld\n", c);
+
+    return 0;
+}
